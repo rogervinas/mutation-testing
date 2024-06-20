dd248793985d4a9c1dcd06d2fbe23fe1604c527f
Roger Viñas Alcon <roger.vinas@fastzink.com>
2024-06-20 13:09:49 +0200

    Refactor gradle (#38)

--- src/test/kotlin/org/rogervinas/MyImplTest.kt
+++ src/test/kotlin/org/rogervinas/MyImplTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 internal class MyImplTest {
-
   @ParameterizedTest
   @CsvSource(
     value = [
@@ -18,9 +17,13 @@ internal class MyImplTest {
       "11, 47, B is greater than A",
       "5, 5, A and B are equal",
       "1, 1, A and B are equal",
-    ]
+    ],
   )
-  fun `should do something`(a: Int, b: Int, expectedResult: String) {
+  fun `should do something`(
+    a: Int,
+    b: Int,
+    expectedResult: String,
+  ) {
     assertThat(MyImpl().doSomething(a, b)).isEqualTo(expectedResult)
   }
 }
