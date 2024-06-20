d840e3488dc471be9002c673786189a018813093
Roger Vinas <roger.vinas@fastzink.com>
2024-06-20 13:06:07 +0200

    Refactor gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,55 +5,62 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("info.solidsoft.pitest") version "1.15.0"
+  kotlin("jvm") version "2.0.0"
+  id("info.solidsoft.pitest") version "1.15.0"
+  id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation(platform("org.junit:junit-bom:5.9.3"))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
+  testImplementation(platform("org.junit:junit-bom:5.9.3"))
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testImplementation("org.junit.jupiter:junit-jupiter-params")
 
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.1")
+  testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.1")
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events(PASSED, SKIPPED, FAILED)
-        exceptionFormat = FULL
-        showExceptions = true
-        showCauses = true
-        showStackTraces = true
-    }
+  useJUnitPlatform()
+  testLogging {
+    events(PASSED, SKIPPED, FAILED)
+    exceptionFormat = FULL
+    showExceptions = true
+    showCauses = true
+    showStackTraces = true
+  }
 }
 
 configure<PitestPluginExtension> {
-    junit5PluginVersion.set("1.0.0")
-    avoidCallsTo.set(setOf("kotlin.jvm.internal"))
-    mutators.set(setOf("STRONGER"))
-    targetClasses.set(setOf("org.rogervinas.*"))
-    targetTests.set(setOf("org.rogervinas.*"))
-    threads.set(Runtime.getRuntime().availableProcessors())
-    outputFormats.set(setOf("XML", "HTML"))
-    mutationThreshold.set(75)
-    coverageThreshold.set(60)
+  junit5PluginVersion.set("1.0.0")
+  avoidCallsTo.set(setOf("kotlin.jvm.internal"))
+  mutators.set(setOf("STRONGER"))
+  targetClasses.set(setOf("org.rogervinas.*"))
+  targetTests.set(setOf("org.rogervinas.*"))
+  threads.set(Runtime.getRuntime().availableProcessors())
+  outputFormats.set(setOf("XML", "HTML"))
+  mutationThreshold.set(75)
+  coverageThreshold.set(60)
 }
 
 tasks.named("check") {
-    dependsOn(":pitest")
+  dependsOn(":pitest")
 }
--- src/main/kotlin/org/rogervinas/MyImpl.kt
+++ src/main/kotlin/org/rogervinas/MyImpl.kt
@@ -1,7 +1,10 @@
 package org.rogervinas
 
 class MyImpl {
-  fun doSomething(a: Int, b: Int) = when {
+  fun doSomething(
+    a: Int,
+    b: Int,
+  ) = when {
     (a < 0 || b < 0) -> "Either A or B are negative"
     (a == 0 && b == 0) -> "Both A and B are zero"
     (a > b) -> "A is greater than B"
